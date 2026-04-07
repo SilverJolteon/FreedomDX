d1a56d5a81bc87a604ea1d3d4d5335b46ab181e9
Silver <silverchrysaetos@live.com>
2026-04-07 12:44:30 -0600

    Fixed save without quitting text

--- build.py
+++ build.py
@@ -6,7 +6,7 @@ import pycdlib
 import subprocess
 from translation.translate import translate, Injector
 
-VERSION = "v1.8.0i DEBUG"
+VERSION = "v1.8.0j DEBUG"
 
 iso_dir = "iso"
 asm_src_dir = "source"
