e0f56577fbcf966c80e5bccf34e0f9780647c801
Silver <silverchrysaetos@live.com>
2026-05-01 07:24:43 -0600

    Moved file replacer code from main

--- source/ULES00318/FileLoader.asm
+++ source/ULES00318/FileLoader.asm
@@ -49,6 +49,59 @@ SetDigit:
 	addiu		sp, sp, 0x20
 	jr			ra
 	nop
+	
+FileLoader:
+	beq			a0, zero, Return
+	nop
+	; Check chunk position
+	lui			t0, 0x2
+	beq			t0, s1, Return
+	nop
+	; Set index
+	jal			FileLoaderSetIndex
+	lh			s1, 0x2(s0)
+	; Open file
+	la			a0, nativePSP
+	li			a1, 0x1
+	jal			sceIoOpen
+	li			a2, 0x0
+	; Check if file exists
+	li			v1, 0x80010002
+	beq			v0, v1, FileLoaderReturn
+	nop	
+	li			v1, 0x0
+	move		s0, v0
+	; Get file size
+	move		a0, s0
+	li			a1, 0x0
+	li			a2, 0x0
+	li			a3, 0x0
+	jal			sceIoLseek
+	li			t0, 0x2
+	beq			v0, zero, FileLoaderReturn
+	nop
+	move		s1, v0
+	; Seek to start of file
+	move		a0, s0
+	li			a1, 0x0
+	li			a2, 0x0
+	li			a3, 0x0
+	jal			sceIoLseek
+	li			t0, 0x0
+	; Read file
+	move		a0, s0
+	la			t0, DEST
+	lw			a1, 0x0(t0)
+	li			t0, 0
+	jal			sceIoRead
+	move		a2, s1
+	; Close file
+	jal			sceIoClose
+	move 		a0, s0
+	jal			sceKDWIA
+	nop
+	j			FileLoaderReturn
+	nop
 		
 nativePSP:
 	.ascii "ms0:/PSP/SAVEDATA/FDXDAT/NATIVEPSP/ULES00318/XXXX"
